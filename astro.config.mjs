2fda963aaae2f01f999ac1b83943b924e14ad1ed
ZiJun Wang <zijundidu@163.com>
2026-02-03 22:41:21 +0800

    add GitHub Actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://zijundidu.github.io',  // 后面改成你的 GitHub Pages 地址
-  // base: '/my-portfolio',  // 仓库名，如果是 username.github.io 则删除这行
+  base: '/my-portfolio',  // 仓库名，如果是 username.github.io 则删除这行
 });
